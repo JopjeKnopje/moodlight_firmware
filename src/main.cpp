c73770bee8e93e0878ab98b02f557abb102f2194
joppe <jboeve009@gmail.com>
2023-08-27 19:40:31 +0200

    started print_color

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
 
-#define PIN_LED_RED 9
-#define PIN_LED_GREEN 10
-#define PIN_LED_BLUE 5
-#define PIN_LED_WHITE 6
+enum PINOUT {
+    PIN_LED_RED,
+    PIN_LED_GREEN,
+    PIN_LED_BLUE,
+    PIN_LED_WHITE,
+    PIN_COUNT,
+};
+
+const static int PINOUT[PIN_COUNT] = {
+    [PIN_LED_RED] = 9,
+    [PIN_LED_GREEN] = 10,
+    [PIN_LED_BLUE] = 5,
+    [PIN_LED_WHITE] = 6,
+};
+
+typedef struct {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t w;
+} ColorRBGW;
+
 
 
 bool is_valid_param(String s)
@@ -16,20 +34,38 @@ bool is_valid_param(String s)
     return true;
 }
 
-
-int parse_serial()
+int parse_serial(ColorRBGW *c)
 {
+    uint8_t *base = &(*c).r;
+    const static int PARAM_COUNT = 4;
     String s = Serial.readString();
-    s.trim();
-    int val;
 
-    if (is_valid_param(s))
+    for (size_t i = 0; i < PARAM_COUNT; i++)
     {
-        val = s.toInt();
-        if (val > 255)
-            val = -1;
+        base[i] = s.substring(0, s.indexOf(',', 1)).toInt();
+        
     }
-    return val;
+    return 1;
+}
+
+// data.remove(data.indexOf(','));
+// Serial.println("X: " + data);
+// return data.toInt();
+
+void set_pwm(ColorRBGW c)
+{
+    uint8_t *base = &c.r;
+    for (int pin : PINOUT)
+    {
+        uint8_t color = (uint8_t) base[pin];
+        analogWrite(pin, color);
+    }
+}
+
+void print_color(ColorRBGW *c)
+{
+    Serial
+
 }
 
 void setup()
@@ -42,17 +78,17 @@ void loop()
 {
     if (Serial.available())
     {
-        int pwm_val = parse_serial();
+        ColorRBGW c;
+        int pwm_val = parse_serial(&c);
         if (pwm_val == -1)
         {
             Serial.println("Error invalid value try [0-255]");
         }
         else
         {
-            Serial.print("Pwm @ ");
+            // set_pwm(c);
+            Serial.print("pwm val: ");
             Serial.println(pwm_val);
-            analogWrite(PIN_LED_GREEN, pwm_val);
-            analogWrite(PIN_LED_RED, pwm_val);
         }
     }
 }
